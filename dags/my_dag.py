d870af847d7c815e36d906f984ef6404831006ba
Hillary Kipkemoi <tenskap@gmail.com>
2024-04-30 08:07:17 +0300

    create more tasks and import operators to carry out tasks

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -1,11 +1,16 @@
 from airflow import DAG
-from airflow.providers.postgres.operators.postgres import PostgresOperator
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.bash import BashOperator
 from datetime import datetime
 from random import randint
 
 def _training_model():
     return randint(1, 10)
+
+def _choose_best_model(ti):
+    if (best_accuracy > 8):
+        return "accurate"
+    return "inaccurate"
 
 # Define the DAG
 with DAG(
@@ -37,4 +42,18 @@ with DAG(
     training_model_C = PythonOperator(
         task_id="training_model_C",
         python_callable=_training_model,
+    )
+
+    choose_best_model = BranchPythonOperator(
+        task_id="choose_best_model",
+    )
+
+    accurate = BashOperator(
+        task_id="accurate",
+        bash_command="echo 'accurate'",
+    )
+
+    inaccurate = BashOperator(
+        task_id="inaccurate",
+        bash_command="echo 'inaccurate'",
     )
