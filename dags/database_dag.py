8c8a7c9ea6da897171c5d7dd6bfb670c0cde21da
Hillary Kipkemoi <tenskap@gmail.com>
2024-04-30 10:17:38 +0300

    create connection for postgres and airflow and engine with sqlachemy

--- dags/database_dag.py
+++ dags/database_dag.py
@@ -4,21 +4,24 @@ from sqlalchemy import create_engine
 import pandas as pd
 from datetime import datetime
 
+def create_postgres_engine(user, password, host, port, db):
+    connection_string = f'postgresql://{user}:{password}@{host}:{port}/{db}'
+    engine = create_engine(connection_string)
+    return engine
+
 def _load_data():
     # Load data into the database
-    # engine = create_engine('postgresql://postgres://postgres:postgres@localhost:5432/postgres')
+    engine = create_postgres_engine('tela', 'tela1234', 'postgres_datawarehouse', '5432', 'traffic')
 
     # Read the csv file
     df = pd.read_csv('/opt/airflow/data/test_data.csv')
-
-        # Rest of your function...
     # Write the data from the DataFrame to the table
-    # df.to_sql('traffic', engine, if_exists='replace', index=False)
+    df.to_sql('traffic', engine, if_exists='replace', index=False)
 
     print("Loading data into the database")
 
 with DAG(
-    dag_id="data_loading_csv",
+    dag_id="data_loading_dev",
     start_date=datetime(2024, 4, 29),
     schedule_interval="@daily",
 ) as dag:
